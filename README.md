b1bb21462e64eb4cfa58a9d14f28721e0ab6d16f
yleniapregnolato <yleniapregnolato@gmail.com>
2024-03-25 15:47:38 +0100

    add: input and first part of logic

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 ## Calcolare il prezzo totale del viaggio
 
 1. INPUT:
-- [ ] chiedere all'utente il numero di chilometri che vuole percorrere
-- [ ] chiedere l'età del passeggero
+- [x] chiedere all'utente il numero di chilometri che vuole percorrere
+- [x] chiedere l'età del passeggero
 
 2. ESECUZIONE LOGICA:
-- [ ] il prezzo del biglietto è definito in base ai km, 0,21€ al km
+- [x] il prezzo del biglietto è definito in base ai km, 0,21€ al km
 - [ ] per i minorenni sconto del 20%
 - [ ] per gli over 65 sconto del 40%
 
--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 // TEST
-alert(ciao)
+// alert("ciao");
+
+//INPUT
+//chiedere all'utente il numero di chilometri che vuole percorrere
+const userKm = prompt("Quanti chilometri devi percorrere?"); //string | null
+console.log(userKm, typeof userKm);
+
+//chiedere l'età del passeggero
+const userAge = prompt("quanti anni hai?") //string | null
+console.log(userAge, typeof userAge);
+
+//ESECUZIONE LOGICA
+//calcolo il prezzo del biglietto in base ai km che l'utente deve percorrere
+userKm = parseInt(userKm); //number
+const price = userKm * 0.21; //number
+
+//applico lo sconto in base all'età
+if (userAge < 18) {
+    price = 
+}
